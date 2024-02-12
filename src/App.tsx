93c7605edc8a745582c7c0ac0a14d6f32f34700b
grega1 <adrianogrego1@gmail.com>
2024-02-12 15:38:42 +0000

    feat: tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { Player } from './pages/Player'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <Provider store={store}>
-      <div>
-       
-      </div>
+      <Player />
      
     </Provider>
   )
